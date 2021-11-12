449e1ea76f41846ef5c0691850f8745cecde619c
auladhimel <himelaulad3021@gmail.com>
2021-11-12 10:10:27 +0600

    7th commit

--- src/Components/Dashboard/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard/Dashboard.js
@@ -15,6 +15,11 @@ import DashboardHome from '../DashboardHome/DashboardHome';
 import MakeAdmin from '../MakeAdmin/MakeAdmin';
 import AddReviews from '../AddReviews/AddReviews';
 import useAuth from '../../../hooks/useAuth';
+import AdminRoute from '../../Login/AdminRoute/AdminRoute';
+import AddProducts from '../AddProducts/AddProducts';
+import ManageOrders from '../ManageOrders/ManageOrders';
+import ManageProducts from '../ManageProducts/ManageProducts';
+import Pay from '../Pay/Pay';
 
 const Dashboard = () => {
     let { path, url } = useRouteMatch();
@@ -24,10 +29,18 @@ const Dashboard = () => {
             <div class="sidenav">
                 <h3>Dashboard</h3>
                 <NavLink to='/home'>Home</NavLink>
-                <NavLink to={`${url}`}>Dashboard</NavLink>
+                <NavLink to={`${url}`}>My Orders</NavLink>
                 {admin && <div>
                     <NavLink to={`${url}/makeAdmin`}>Make Admin</NavLink>
-                    <NavLink to={`${url}/addReviews`}>Add Reviews</NavLink></div>}
+                    <NavLink to={`${url}/addProducts`}>Add Products</NavLink>
+                    <NavLink to={`${url}/manageOrders`}>Manage Orders</NavLink>
+                    <NavLink to={`${url}/manageProducts`}>Manage Products</NavLink>
+                </div>}
+                {!admin && <div>
+                    <NavLink to={`${url}/addReviews`}>Add Review</NavLink>
+                    <NavLink to={`${url}/pay`}>Pay</NavLink>
+                </div>}
+                <NavLink to={`${url}/logout`}>Logout</NavLink>
             </div>
 
             <div>
@@ -35,12 +48,24 @@ const Dashboard = () => {
                     <Route exact path={path}>
                         <DashboardHome></DashboardHome>
                     </Route>
-                    <Route path={`${path}/makeAdmin`}>
-                        <MakeAdmin></MakeAdmin>
-                    </Route>
                     <Route path={`${path}/addReviews`}>
                         <AddReviews></AddReviews>
                     </Route>
+                    <Route path={`${path}/pay`}>
+                        <Pay></Pay>
+                    </Route>
+                    <AdminRoute path={`${path}/makeAdmin`}>
+                        <MakeAdmin></MakeAdmin>
+                    </AdminRoute>
+                    <AdminRoute path={`${path}/addProducts`}>
+                        <AddProducts></AddProducts>
+                    </AdminRoute>
+                    <AdminRoute path={`${path}/manageOrders`}>
+                        <ManageOrders></ManageOrders>
+                    </AdminRoute>
+                    <AdminRoute path={`${path}/manageProducts`}>
+                        <ManageProducts></ManageProducts>
+                    </AdminRoute>
                 </Switch>
             </div>
         </Container>
