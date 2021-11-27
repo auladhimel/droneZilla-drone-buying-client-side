7ecc693375a3c69d600653f9b4996c5c5e0c3c91
auladhimel <himelaulad3021@gmail.com>
2021-11-27 16:50:54 +0600

    readme file added

--- src/Components/Dashboard/DashboardHome/DashboardHome.js
+++ src/Components/Dashboard/DashboardHome/DashboardHome.js
@@ -7,8 +7,6 @@ import dashboard from '../../../images/dashboard.jpeg';
 const DashboardHome = () => {
     return (
         <div className="main ms-5 ps-5">
-            <h2 style={{ paddingTop: "30px", marginBottom: "20px", marginleft: "20px", fontSize: "30px", fontWeight: "bold" }}>Dashboard</h2>
-            <img style={{ borderRadius: "10px", width: "50%" }} src={dashboard} alt="" />
             <Orders></Orders>
         </div>
     );
