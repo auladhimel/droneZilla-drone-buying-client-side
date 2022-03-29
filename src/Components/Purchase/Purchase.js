2cc5adbb36dfee7e04483ae1a13d71eae4653ec6
auladhimel <himelaulad3021@gmail.com>
2022-03-30 02:32:14 +0600

    Make order fixed

--- src/Components/Purchase/Purchase.js
+++ src/Components/Purchase/Purchase.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import './Purchase.css';
@@ -11,26 +12,28 @@ const Purchase = () => {
     const { user } = useAuth();
     const initialInfo = { customerName: user.displayName, email: user.email, phone: '', status:'Pending' }
     const [orderInfo, setOrderInfo] = useState(initialInfo);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-    const handlePurchasingSubmit = e => {
-        console.log(orderInfo);
-        fetch(`http://localhost:5000/orders/${productId}`,{
-            method: "POST",
-            headers: { 
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(orderInfo),
-        })
+    // const [orderSuccess, setOrderSuccess]= useState(false);
+    // const handlePurchasingSubmit = e => {
+    //     console.log(orderInfo);
+    //     fetch(`http://localhost:5000/orders/${productId}`,{
+    //         method: "POST",
+    //         headers: { 
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(orderInfo),
+    //     })
         
-        .then((res) => res.json())
-        .then(data => {
-            if (data.insertedId) {
-                alert('order placed');
-                e.target.reset();
-            }
-        })
+    //     .then((res) => res.json())
+    //     .then(data => {
+    //         if (data.insertedId) {
+    //             alert('order placed');
+    //             e.target.reset();
+    //         }
+    //     })
     
-      }
+    //   }
 
 
     // const handlePurchasingSubmit = e => {
@@ -71,37 +74,94 @@ const Purchase = () => {
         setOrderInfo(newInfo);
 
     }
-    const handleSuccessAlert = e => {
-        alert('success');
-        e.preventDefault();
+
+
+    const onSubmit=e=>{
+        const order={
+            ...orderInfo,
+            customerName: user.displayName, 
+            email: user.email, 
+            phone: '',
+            address:'', 
+            status:'Pending',
+          
+        }
+        console.log(order);
+        fetch('http://localhost:5000/orders',{
+            method: 'POST',
+            headers:{ 
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.insertedId){  
+                alert('Order Placed Successfully');
+                
+            }
+        })
     }
+
     return (
-        <div className="purchase">
-            <h1 style={{ paddingTop: "20px", marginBottom: "30px", fontSize: "28px", fontWeight: "bold" }}>Purchase</h1>
-            <Form onSubmit={handlePurchasingSubmit}>
-                <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3">
-                    <Form.Control type="text" onBlur={handleOnBlur} name="customerName" placeholder="Your Name" value={user.displayName} />
-                </Form.Group>
-                <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3">
-                    <Form.Control type="email" onBlur={handleOnBlur} placeholder="Your Email" value={user.email} />
-                </Form.Group>
-                <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3" >
-                    <Form.Control type="number" onBlur={handleOnBlur} name="phone" placeholder="Phone Number" />
-                </Form.Group>
-                <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3" >
-                    <Form.Control type="text" onBlur={handleOnBlur} name="address" placeholder="Address" />
-                </Form.Group>
-                <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3" >
-                    <Form.Control type="text" onBlur={handleOnBlur} name="product" value={orderInfo.productName} placeholder="Product" />
-                </Form.Group>
-                <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3" >
-                    <Form.Control type="number" onBlur={handleOnBlur} name="price" value={orderInfo.price} placeholder="Price" />
-                </Form.Group>
-                <Button variant="warning" type="submit">
-                    Place Order
-                </Button>
-            </Form>
-        </div >
+        <div className="updateProduct">
+        <h2>Make Order</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+  {/* register your input into the hook by invoking the "register" function */}
+  <label style={{ paddingRight:"75px"}}>Name : </label>
+  <input style={{ marginBottom:"20px"}} defaultValue={user.displayName}  {...register("status")}/> <br />
+
+  <label style={{ paddingRight:"75px"}}>Email : </label>
+  <input style={{ marginBottom:"20px"}} defaultValue={user.email}  {...register("email")}/> <br />
+
+  <label style={{ paddingRight:"75px"}}>Phone : </label>
+  <input style={{ marginBottom:"20px"}} placeholder="Phone"  {...register("phone")}/> <br />
+
+  <label style={{ paddingRight:"50px"}}>Address : </label>
+  <input style={{ marginBottom:"20px"}} placeholder="Address" {...register("address")}/> <br />
+
+  <label style={{ paddingRight:"10px"}}>Product Name : </label>
+  <input style={{ marginBottom:"20px"}} name="firstName" defaultValue={orderInfo.productName} {...register("productName")} /> <br />
+
+  <label style={{ paddingRight:"75px"}}>Price : </label>
+  <input style={{ marginBottom:"20px"}} defaultValue={orderInfo.price}  {...register("price")}/> <br />
+  
+  
+  {/* errors will return when field validation fails  */}
+  {errors.exampleRequired && <span>This field is required</span>}
+  
+  <input style={{ marginLeft:"90px"}} variant="warning" className="submit" type="submit"/>
+</form>
+
+{/* {orderSuccess && <alert>Order Placed Successfully!</alert>} */}
+    </div>
+        // <div className="purchase">
+        //     <h1 style={{ paddingTop: "20px", marginBottom: "30px", fontSize: "28px", fontWeight: "bold" }}>Purchase</h1>
+        //     <Form onSubmit={handleOrderSubmit}>
+        //         <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3">
+        //             <Form.Control type="text" onBlur={handleOnBlur} name="customerName" placeholder="Your Name" value={user.displayName} />
+        //         </Form.Group>
+        //         <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3">
+        //             <Form.Control type="email" onBlur={handleOnBlur} placeholder="Your Email" value={user.email} />
+        //         </Form.Group>
+        //         <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3" >
+        //             <Form.Control type="number" onBlur={handleOnBlur} name="phone" placeholder="Phone Number" />
+        //         </Form.Group>
+        //         <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3" >
+        //             <Form.Control type="text" onBlur={handleOnBlur} name="address" placeholder="Address" />
+        //         </Form.Group>
+        //         <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3" >
+        //             <Form.Control type="text" onBlur={handleOnBlur} name="product" value={orderInfo.productName} placeholder="Product" />
+        //         </Form.Group>
+        //         <Form.Group style={{ width: '30%', margin: 'auto' }} className="mb-3" >
+        //             <Form.Control type="number" onBlur={handleOnBlur} name="price" value={orderInfo.price} placeholder="Price" />
+        //         </Form.Group>
+        //         <Button variant="warning" type="submit">
+        //             Place Order
+        //         </Button>
+        //     </Form>
+        // </div >
     );
 };
 
